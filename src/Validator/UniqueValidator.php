5fc57132d1f93a2440a16ce1d25ed3cd875c647c
Alexandr Satyshev <kptlex@gmail.com>
2021-09-13 16:07:01 +0300

    Feature: add unique validator for cycle entities

---
+++ src/Validator/UniqueValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lex\Yii\Cycle\Validator;
+
+use Cycle\ORM\ORMInterface;
+use Yii;
+use yii\validators\Validator;
+
+final class UniqueValidator extends Validator
+{
+    public string $role;
+    public ?string $field;
+    private ORMInterface $orm;
+
+    public function __construct(ORMInterface $orm, $config = [])
+    {
+        $this->orm = $orm;
+        $this->role = '';
+        $this->field = null;
+        parent::__construct($config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAttribute($model, $attribute): void
+    {
+        $repository = $this->orm->getRepository($this->role);
+        $entity = $repository->findOne([$this->field ?: $attribute => $model->$attribute]);
+        if ($entity) {
+            $this->addError($model, $attribute, Yii::t('yii', '{attribute} is invalid.'));
+        }
+    }
+}
